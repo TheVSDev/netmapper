4e532a3e36fd367cf720795953b056b65ebaa936
TheVSDev <vspasic03@gmail.com>
2023-05-08 21:03:38 +0200

    fix: removing env.local and fixing db connection

--- src/api/mw.js
+++ src/api/mw.js
@@ -12,7 +12,7 @@ const mw = (handlersByMethod) => async (req, res) => {
     return
   }
 
-  await mongoose.connect(process.env.MONGO_URI)
+  await mongoose.connect(config.db.uri)
 
   try {
     let handlerIndex = 0
