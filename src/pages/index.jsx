86bb0acce50933fcae9415333f4ae838e5710b99
TheVSDev <vspasic03@gmail.com>
2023-05-19 21:04:09 +0200

    done: welcome page span underline style

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,14 +27,14 @@ export default function Welcome() {
             <h1 className={styles.title}>Net Mapper</h1>
             <br />
             <p className={styles.text}>
-              Welcome to Network Mapper online tool. <br />
+              Welcome to <span className={styles.underlineGradient}>Network Mapping</span> online tool. <br />
               It is an online web interface for a command line tool <code className={styles.code}>nmap</code>. <br />
-              Let&apos;s map the network together. <br />
-              Sign in your account and if you don&apos;t have one, create it.
+              <span className={styles.underlineGradient}>Let&apos;s map the network together.</span> <br />
+              <span className={styles.underlineWavy}>Sign in</span> your account to save your scans or if you don&apos;t have one, <span className={styles.underlineWavy}>create it</span>.
             </p>
             <br />
             <br /> 
-            <Link href="/scan-page"><Button btnLabel="Let's map the network" /></Link><br /><br />
+            <Link href="/scan-page"><Button btnLabel="Continue as a guest" /></Link><br /><br />
             <Link href="/sign-in"><Button btnLabel="Log into your account" /></Link><br /><br />
             <Link href="/sign-up"><Button btnLabel="Create an account" /></Link>
         </div>
